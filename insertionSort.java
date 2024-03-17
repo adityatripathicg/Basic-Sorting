ad2852693605e77f4af766e9ceb3b86d3d3caf8f
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-03-18 00:10:08 +0530

    sort

--- insertionSort.java
+++ insertionSort.java
@@ -1,5 +1,5 @@
 public class insertionSort {
-    public static void insertionSort(int arr[]){
+    public static void insertionS(int arr[]){
         for(int i = 0;i<=arr.length-1;i++){ //i <= length-1 or i < length both work
             int j = i;
             while(j>0 && arr[j-1]>arr[j]){
@@ -17,7 +17,7 @@ public class insertionSort {
     }
     public static void main(String args[]){
         int arr []= { 5, 4, 1, 3, 2 };
-        insertionSort(arr);
+        insertionS(arr);
         printArr(arr);
     }
 }
